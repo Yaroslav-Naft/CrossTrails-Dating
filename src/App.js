1101c89b031dc87330783ae67a6b90b408cf4b84
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-03-08 13:20:36 -0800

    Added routing to main page

--- src/App.js
+++ src/App.js
@@ -6,22 +6,17 @@ import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <Router>
-    <span>
-      <ul>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Sign up</Link></li>
-      </ul>
-    </span>
-
-
-    <Switch>
-    <Route exact path="/signup"><Signup onSubmit={(data) => console.log("submit signup", data)}/></Route>
-    <Route exact path="/login"><Login onSubmit={(data) => console.log("submit login", data)}/></Route>
-    <Route path="/"></Route>
-    </Switch>
-
-
-
+      <span>
+        <ul>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Sign up</Link></li>
+        </ul>
+      </span>
+      <Switch>
+        <Route exact path="/signup"><Signup onSubmit={(data) => console.log("submit signup", data)}/></Route>
+        <Route exact path="/login"><Login onSubmit={(data) => console.log("submit login", data)}/></Route>
+        <Route path="/"><Login onSubmit={(data) => console.log("submit login", data)}/></Route>
+      </Switch>
     </Router>
   );
 }
