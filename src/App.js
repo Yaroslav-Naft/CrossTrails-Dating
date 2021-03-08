206ab336505b0d2417e2f51253709ba59d8cb53b
daveroen <daveroenb@gmail.com>
2021-03-08 13:00:48 -0800

    linking navbar to app js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 // Components
 import Login from "./components/Login"
 import Signup from "./components/Signup/index"
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import GuardedRoute from './components/auth/GuardedRoute'
+import Home from './components/Protected';
+
+export default function App() {
+  const [isAuthenticaed, setisAuthenticated] = useState(false);
+
+  //authenticate user going to is Authenticated 
+  const authenticateUser = (authstate) => {
+    setisAuthenticated(authState)
+  }
+}
+
+useEffect(() => {
+  console.log('Authenticated: $(isAuthenticated');
+})
+
+return (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to='/'>Home page</Link></li>
+        <li><Link to='/'>Protected Page</Link></li>
+        <li><Link to='/'>Register</Link></li>
+        <li><Link to='/'>Login</Link></li>
+      </ul>
+    </div>
+  </Router>
+)
+
+export default App;
+
 
 function App() {
   return (
-    <div className="App">
-     <Signup onSubmit={(data) => console.log("submit signup", data)}/>
-     <Login onSubmit={(data) => console.log("submit login", data)}/>
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component ={Home} />
+          <Route path='/' exact component ={About} />
+          <Route path='/matches' exact component={Matches} />
+          <Route path="/profile" exact component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
+const Home  = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
+const Matches  = () => <h1>Matches</h1>;
+const Profile = () => <h1>Profile</h1>;
 
-export default App;
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,30 +1,22 @@
 import React, { Component } from 'react';
 import { MenuItems } from "./MenuItems";
+import { Switch, Route, Link} from "react=router-dom";
+
 
 
 class Navbar extends Component {
+    state = {};
     render() {
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">React</h1>
                 <div className="menu-icon"></div>
-                <ul>
-                
-                { MenuItems.map((item, index) => {
-                    return (
-                        <li key={index}>
-                            <a className={item.cName} href={item.url}>
-                                {item.title}
-                            </a>
-                        </li>
-                    )
-                })}
-
-                    <li><a className={item.cName}></a></li>
-                </ul>
+                <Nav.Link href='/'>Home</Nav.Link>
+                <Nav.Link href='/'>About</Nav.Link>
+                <Nav.Link href='/'>Updates</Nav.Link>
+                <Nav.Link href='/'>Profile</Nav.Link>
             </nav>
         )
     }
 }
 
-export dafault NavBar
