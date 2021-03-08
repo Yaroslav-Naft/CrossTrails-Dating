459b079ace0a11094b919fa00b88a874313dff2d
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-03-08 15:50:27 -0800

    integrating api gateway to react app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Login from "./components/Login"
 import Signup from "./components/Signup/index"
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Settings from "./components/Settings";
 
 function App() {
   return (
@@ -10,11 +11,13 @@ function App() {
         <ul>
           <li><Link to="/login">Login</Link></li>
           <li><Link to="/signup">Sign up</Link></li>
+          <li><Link to="/settings">Settings</Link></li>
         </ul>
       </span>
       <Switch>
         <Route exact path="/signup"><Signup onSubmit={(data) => console.log("submit signup", data)}/></Route>
         <Route exact path="/login"><Login onSubmit={(data) => console.log("submit login", data)}/></Route>
+        <Route exact path="/settings"><Settings /></Route>
         <Route path="/"><Login onSubmit={(data) => console.log("submit login", data)}/></Route>
       </Switch>
     </Router>
---
+++ src/components/Settings/index.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from "react";
+
+
+export default function Settings() {
+
+const url = "https://o6wwobyn4k.execute-api.us-west-1.amazonaws.com/prod"
+const [hikerData, sethikerData] = useState([])
+
+
+const displaySettings = async event => {
+    fetch(url + '/hikers')
+    .then(response => response.json)
+    .then(data => {
+        sethikerData(JSON.parse(data.body))
+    })
+
+
+}
+
+
+useEffect(() => {
+    displaySettings()
+}, [])
+
+
+    return (
+<div>
+  <h2>Hikers</h2>
+  <table>
+      <thead>
+          <tr>
+              <th>Id</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            {hikerData.map(hiker =>
+            <tr key={hiker.hikersId}>
+                <td>{hiker.age}</td>
+                <td>{hiker.favouritesHikes}</td>
+            </tr>
+            )}
+        </tbody>
+    </table>
+</div>
+
+        
+    )
+}
+
