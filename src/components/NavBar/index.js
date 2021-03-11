48584490e71f1ed156e3075a739d3955ffc75313
daveroen <daveroenb@gmail.com>
2021-03-11 11:08:49 -0800

    fix navbar error

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -12,15 +12,7 @@ class Navbar extends Component {
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">React</h1>
                 <div className="menu-icon"></div>
-<<<<<<< HEAD
-                <Nav.Link href='/'>Home</Nav.Link>
-                <Nav.Link href='/'>About</Nav.Link>
-                <Nav.Link href='/'>Updates</Nav.Link>
-                <Nav.Link href='/'>Profile</Nav.Link>
-=======
                 <ul>
-
-                
                 { MenuItems.map((item, index) => {
                     return (
                         <li key={index}>
@@ -34,13 +26,9 @@ class Navbar extends Component {
 
                     <li><a className={item.cName}></a></li>
                 </ul>
->>>>>>> 8ea8e45d1f8134983398a7f054406c5af0ca573f
             </nav>
         )
     }
 }
 
-<<<<<<< HEAD
-=======
 export default NavBar
->>>>>>> 8ea8e45d1f8134983398a7f054406c5af0ca573f
