ed6f7362f3ee182c271f410745c3be2b12c4aa11
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-03-21 11:35:54 -0700

    working on deleting likes

--- src/components/Likes/index.js
+++ src/components/Likes/index.js
@@ -40,7 +40,8 @@ const [like, setLike] = useState( {like: { senderUserName: 'yaro', targetUserNam
 //const [likesId, setLikesId] = useState("")
 const [senderUserName, setSenderUserName] = useState("sender Username")
 const [targetUserName, setTargetUserName] = useState("target Username")
-
+const [likesList, setLikesList] = useState([])
+const [deleteLikesId, setDeleteLikesId] = useState("1f042a70-89fd-11eb-b826-0f0160bc07dc")
 
 const addLike = s => {
     s.preventDefault()
@@ -64,18 +65,63 @@ const addLike = s => {
 // }
 
 
+//Display hikers
+
+// const displaySettings = async () => {
+//     fetch(`${url}/hikers/${state.username}`)
+//     .then(response => response.json())
+//     .then(data => {
+//         setHiker(data)
+//     })
+//     }   
+
+
+//Delete Like
+
+// const deleteLike = s => {
+//     s.preventDefault()
+//      fetch( url + `/like/${senderUserName}`, {
+//         method: 'PUT',
+//         body: JSON.stringify( {"likesId: } ),
+//         headers: { 'Content-Type': 'application/json'}
+//     })
+//     //update body
+//     .then(response => (response.json(), console.log(response)))
+//     //.then(() => { displaySettings ()} )
+// }
+
+
+const deleteLike = s => {
+    //console.log({deleteLikesId})
+    s.preventDefault()
+    //pass product
+    fetch(url + `/like/${deleteLikesId}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json'}
+    })
+    history.push("/")
+}
+
+
+
 //with path
 const getLike = async () => {
     fetch(`${url}/likes/${senderUserName}`)
     .then(response => response.json())
     .then(data => {
-        console.log({data})
+        //console.log({data})
+        setLikesList(data)
         //setHiker(data)
+        //console.log({likesList})
         //setLoading(0)
     })
     }   
 
-
+    useEffect(() => {
+        //const test = likesList[1]
+        console.log(likesList)
+        console.log(`printed`)
+    }, [likesList])
 
 
 
@@ -85,7 +131,6 @@ useEffect( async () => {
     }).then(user => 
         setSenderUserName(user.username)
     )
-    .then( console.log('now running'))
     .catch();
 
     setLike({like: { senderUserName: senderUserName, targetUserName: 'maria' }} )
@@ -96,16 +141,34 @@ useEffect(() => {
  setLike({like: { senderUserName: senderUserName, targetUserName: 'maria' }} )
 }, [senderUserName])
 
-console.log({senderUserName})
-console.log({like})
 
     return (
         <div>
                 <Button onClick={addLike}>Add Like</Button>
 
                 <h2>All likes for current User</h2>
+
+
+                <table>
+                <thead>
+                    <tr>
+                        <th>Users Who Liked you</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {likesList.map(like =>
+                <tr key={like.LikesId}> 
+                    <td>{like.TargetUserName}</td>
+                    <Button onClick={ deleteLike}>Delete Like</Button>
+                </tr>          
+                )}
+                </tbody>
+            </table>
+                
                 
                 <Button onClick={getLike}>Get Like</Button>
+
+                
 
 
         </div>
